8b1141c6df810a9010c97b0b250a65dab5cdcaf6
Chiraag Anand <chiraag@Lis-MacBook-Pro.local>
2021-05-06 16:51:09 +0800

    init

---
+++ src/components/Book.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <image src="http://placeimg.com/480/640/any" alt="Book cover image" />
+    <div>Title: {{ title }}</div>
+    <div>Author: {{ author }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Book",
+  props: {
+    title: String,
+    author: String,
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,90 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-if="displayName && firstName">WELCOME {{ displayName }}</div>
+    <div v-if="displayName && lastName">
+      WELCOME {{ isMale ? "MR" : "MS" }} {{ displayName }}
+    </div>
+    <div>
+      <input type="checkbox" id="checkbox" v-model="isMale" />
+      <label for="checkbox">Are you Male??</label>
+    </div>
+    <template v-if="isInputFirstName">
+      <label>First Name</label>
+      <input
+        v-model="firstName"
+        placeholder="Enter your first name"
+        key="firstname"
+      />
+    </template>
+    <template v-else>
+      <label>Last Name</label>
+      <input
+        v-model="lastName"
+        placeholder="Enter your last name"
+        key="lastname"
+      />
+    </template>
+    <button v-on:click="getBookRecomendations()">
+      Get your book reconmendation
+    </button>
+    <button v-on:click="isInputFirstName = !isInputFirstName">
+      Toggle input field
+    </button>
+    <template v-if="bookRecomendations.length !== 0">
+      <Book
+        :title="bookRecomendations[0].title"
+        :author="bookRecomendations[0].author"
+      />
+    </template>
   </div>
 </template>
 
 <script>
+import Book from "./Book";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: { Book },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data: () => {
+    return {
+      firstName: "",
+      lastName: "",
+      isInputFirstName: true,
+      isMale: true,
+      bookRecomendations: [],
+    };
+  },
+  computed: {
+    displayName: function() {
+      if (this.firstName) {
+        return this.firstName.toUpperCase();
+      } else if (this.lastName) {
+        return this.lastName.toUpperCase();
+      } else return "";
+    },
+  },
+  methods: {
+    getBookRecomendations: async function() {
+      if (!this.firstName && !this.lastName) {
+        alert(
+          "Tell us your first name or last name before we recomend a book!"
+        );
+        return;
+      }
+      try {
+        const res = await fetch("https://fakerapi.it/api/v1/books?_quantity=1");
+        const data = await res.json();
+        console.log(data.data);
+        this.bookRecomendations = data.data;
+      } catch (error) {
+        alert(error.message);
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
